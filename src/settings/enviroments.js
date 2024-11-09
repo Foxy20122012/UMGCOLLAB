9eed66180f5d7f2c60c8b92636e61a86964281d9
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-11-09 01:38:58 -0600

    first commit

--- src/settings/enviroments.js
+++ src/settings/enviroments.js
@@ -2,7 +2,7 @@
 
 
 //const environment = "http://localhost:3000";
-const environment = "http://198.7.123.157:4000/";
+const environment = "http://locahost:3000/";
 //const environment = process.env.NEXT_PUBLIC_API_URL;
 
 module.exports = {
